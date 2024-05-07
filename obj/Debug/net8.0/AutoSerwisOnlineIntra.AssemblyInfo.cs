00fe666b4eb4e4dc1875ee22b19afab060b5078e
Klaudia <kdziubek@happcode.com>
2024-05-08 00:07:26 +0100

    Styled car parts page

--- obj/Debug/net8.0/AutoSerwisOnlineIntra.AssemblyInfo.cs
+++ obj/Debug/net8.0/AutoSerwisOnlineIntra.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoSerwisOnlineIntra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef347b12b1295316a84e6ee6bf4f8828a1436192")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoSerwisOnlineIntra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoSerwisOnlineIntra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
